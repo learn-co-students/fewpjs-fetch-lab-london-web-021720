3568780ce4519f9b5fb858f1e1c6c080d918e403
Amaterasus <skyetay03@googlemail.com>
2020-06-08 13:52:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-function fetchBooks() {
+const main = document.querySelector('main')
+
+const fetchBooks = () => {
   return fetch("https://anapioficeandfire.com/api/books")
   .then(resp => resp.json())
-  .then(json => renderBooks(json))
+  .then(books => renderBooks(books))
 }
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+const renderBooks = (json) => {
+  json.forEach(book => renderBook(book))
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+const renderBook = book => {
+  const h2 = document.createElement('h2')
+  h2.innerText = book.name
+  main.appendChild(h2)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   fetchBooks()
 })
